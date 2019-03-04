3cf13396b5e175a313de9fb6f8702654e6539cb7
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-03-04 11:43:53 +0000

    Added user input for positon on board

--- game.c
+++ game.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BOARD_SIZE 3
 
 void displayBoard(int[BOARD_SIZE][BOARD_SIZE]);
+int updateBoard(int board[BOARD_SIZE][BOARD_SIZE], int, int);
 
 int main(void)
 {
@@ -10,7 +12,65 @@ int main(void)
     
     int board[BOARD_SIZE][BOARD_SIZE] = {0};
     displayBoard(board);
+
+    int gameOver = -1;
+    int count = 0;
+    int currentPlayer = 0;
+    while(gameOver == -1)
+    {
+        if(count % 2 == 0)
+        {
+            printf("(X) Enter a position 1-9\n");
+            currentPlayer = 1;
+        }
+        else
+        {
+            printf("(O) Enter a position 1-9\n");
+            currentPlayer = 2;
+        }
+        
+        char *end;
+        char buf[5];
+
+        do {
+            if (!fgets(buf, sizeof buf, stdin))
+            {
+                break;
+            }
+
+            // Strip the newline character (\n)
+            buf[strlen(buf) - 1] = 0;
+
+            int input = strtol(buf, &end, 10);
+            if(input > 0 && input <= (BOARD_SIZE * BOARD_SIZE))
+            {
+                int valid = updateBoard(board, input, currentPlayer);
+                if(valid != -1)
+                {
+                    displayBoard(board);
+                    count += 1;
+                }
+                else
+                {
+                    printf("Position %d is already taken\n", input);
+                }
+            }
+            else
+            {
+                printf("Invalid input\n");
+            }
+        }
+        while (end != buf + strlen(buf));
+    }
+
     return 0;
+}
+
+int updateBoard(int board[BOARD_SIZE][BOARD_SIZE], int position, int value)
+{
+    return 0;
+
+    // return -1 if position is already taken
 }
 
 void displayBoard(int board[BOARD_SIZE][BOARD_SIZE])
@@ -34,7 +94,18 @@ void displayBoard(int board[BOARD_SIZE][BOARD_SIZE])
         // Block value
         for (int i=0; i < BOARD_SIZE; i++)
         {
-            printf("|  %d  |", board[idx][i]);
+            if (board[idx][i] == 0)
+            {
+                printf("|     |", board[idx][i]);
+            }
+            else if (board[idx][i] == 1)
+            {
+                printf("|  X  |", board[idx][i]);
+            }
+            else if (board[idx][i] == 2)
+            {
+                printf("|  O  |", board[idx][i]);
+            }
         }
         printf("\n");
 
