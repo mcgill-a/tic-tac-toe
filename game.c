dd80d6ef48ab765c620d1423bd8af205dbf7bf46
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-03-04 19:06:07 +0000

    Implemented player moves onto the board

--- game.c
+++ game.c
@@ -8,8 +8,6 @@ int updateBoard(int board[BOARD_SIZE][BOARD_SIZE], int, int);
 
 int main(void)
 {
-    printf("Tic Tac Toe (%dx%d)\n", BOARD_SIZE, BOARD_SIZE);
-    
     int board[BOARD_SIZE][BOARD_SIZE] = {0};
     displayBoard(board);
 
@@ -18,14 +16,20 @@ int main(void)
     int currentPlayer = 0;
     while(gameOver == -1)
     {
+        if (count == BOARD_SIZE * BOARD_SIZE)
+        {
+            gameOver = 1;
+            break;
+        }
+
         if(count % 2 == 0)
         {
-            printf("(X) Enter a position 1-9\n");
+            printf("(X) Enter a position 1-%d\n", BOARD_SIZE);
             currentPlayer = 1;
         }
         else
         {
-            printf("(O) Enter a position 1-9\n");
+            printf("(O) Enter a position 1-%d\n", BOARD_SIZE);
             currentPlayer = 2;
         }
         
@@ -68,13 +72,29 @@ int main(void)
 
 int updateBoard(int board[BOARD_SIZE][BOARD_SIZE], int position, int value)
 {
-    return 0;
-
-    // return -1 if position is already taken
+    // position / BOARD_SIZE (rounded to nearest whole number)
+    // Subtract 1 because arrays start at 0
+    int row = ((position -1) / BOARD_SIZE + 1) - 1;
+    // 
+    int column = (BOARD_SIZE -1) - (((row+1) * BOARD_SIZE) - position);
+    
+    // If the position is not already occupied
+    if (board[row][column] == 0)
+    {
+        board[row][column] = value;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 void displayBoard(int board[BOARD_SIZE][BOARD_SIZE])
 {
+    system("cls");
+    printf("Tic Tac Toe (%dx%d)\n", BOARD_SIZE, BOARD_SIZE);
+
     // Top edge
     for (int i=0; i < BOARD_SIZE; i++)
     {
@@ -82,6 +102,7 @@ void displayBoard(int board[BOARD_SIZE][BOARD_SIZE])
     }
     printf("\n");
 
+    int counter = 0;
     for (int idx=0; idx < BOARD_SIZE; idx++)
     {
         // Spacer 1
@@ -94,9 +115,11 @@ void displayBoard(int board[BOARD_SIZE][BOARD_SIZE])
         // Block value
         for (int i=0; i < BOARD_SIZE; i++)
         {
+            counter += 1;
             if (board[idx][i] == 0)
             {
-                printf("|     |", board[idx][i]);
+                printf("|     |");
+                //printf("|  %d  |", counter);
             }
             else if (board[idx][i] == 1)
             {
